59c2bd6d2747d2e6d7a4ac41ced90153fc954994
HP <HP@LAPTOP-MOLJOM71>
2021-12-11 14:35:16 +0530

    Guru

---
+++ Sample_Maven_Project/src/test/java/Test/Sample_Maven_Project/Tc01Test.java
@@ -0,0 +1,17 @@
+package Test.Sample_Maven_Project;
+
+import org.testng.annotations.Test;
+
+public class Tc01Test {
+	@Test
+	public void Webshop(){
+		System.out.println("running webshop");
+	}
+	
+	@Test
+	public void Medicalshop(){
+		System.out.println("running medicalshop");
+	}
+
+	
+}
---
+++ Sample_Maven_Project/src/test/java/Test/Sample_Maven_Project/Tc02Test.java
@@ -0,0 +1,16 @@
+package Test.Sample_Maven_Project;
+
+import org.testng.annotations.Test;
+
+public class Tc02Test {
+	@Test
+	public void panshop(){
+		System.out.println("running panshop");
+	}
+	
+	@Test
+	public void clothshop(){
+		System.out.println("running clothshop");
+	}
+
+}
